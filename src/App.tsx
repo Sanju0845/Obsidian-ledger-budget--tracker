00af4dc939899d8f8177ae215e720b228787bf47
Sanju0845 <150457051+Sanju0845@users.noreply.github.com>
2026-04-05 21:45:47 +0530

    feat: Add Airtel UPI app to list
    Includes Airtel UPI app with its package name and icon, expanding the available UPI app integrations.

--- src/App.tsx
+++ src/App.tsx
@@ -875,9 +875,9 @@ const QRScanner = ({ onScan, onClose }: { onScan: (data: string) => void, onClos
 };
 
 const UPI_APPS = [
-  { name: 'PhonePe', package: 'com.phonepe.app', icon: 'https://img.icons8.com/?size=48&id=OYtBxIlJwMGA&format=png' },
   { name: 'Paytm', package: 'net.one97.paytm', icon: 'https://img.icons8.com/?size=48&id=68067&format=png' },
   { name: 'Google Pay', package: 'com.google.android.apps.nbu.paisa.user', icon: 'https://img.icons8.com/?size=48&id=am4ltuIYDpQ5&format=png' },
+  { name: 'Airtel', package: 'com.myairtelapp', icon: 'https://cdn.iconscout.com/icon/free/png-512/free-airtel-icon-svg-download-png-14551356.png?f=webp&w=256' },
   { name: 'FamPay', package: 'com.fampay.app', icon: 'https://play-lh.googleusercontent.com/0IkegIm8uvzIM3RiVBfj01eSlNa3r5C_GCuExyI57b9_x-qLeV8YR3SVuT8DPPYT_N0=w240-h480-rw' },
   { name: 'Amazon Pay', package: 'in.amazon.mShop.android.shopping', icon: 'https://play-lh.googleusercontent.com/urVIq3KHpF9hAm7FJpE2I4YlGfqMFpUdb5GMtQcASC1ODbWe1zuQFrF99ZPTELfE8wA=w240-h480-rw' },
   { name: 'BHIM', package: 'in.org.npci.upiapp', icon: 'https://play-lh.googleusercontent.com/B5cNBA15IxjCT-8UTXEWgiPcGkJ1C07iHKwm2Hbs8xR3PnJvZ0swTag3abdC_Fj5OfnP=w240-h480-rw' },
